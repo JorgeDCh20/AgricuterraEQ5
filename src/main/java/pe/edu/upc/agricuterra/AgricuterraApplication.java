7a5718b0b51a68f82fed11d8d62328fa2900b0a6
emersondazu <104316865+emersondazu@users.noreply.github.com>
2022-06-01 23:51:05 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/upc/agricuterra/AgricuterraApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.agricuterra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgricuterraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgricuterraApplication.class, args);
+	}
+
+}
