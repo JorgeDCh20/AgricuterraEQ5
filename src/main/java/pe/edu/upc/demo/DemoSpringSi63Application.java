3c1088dc8bd8a38dd449c3a8a2309f39094a9b9e
CristianVillalobos17712 <104316342+CristianVillalobos17712@users.noreply.github.com>
2022-06-01 23:40:06 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/upc/demo/DemoSpringSi63Application.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSi63Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringSi63Application.class, args);
+	}
+
+}
